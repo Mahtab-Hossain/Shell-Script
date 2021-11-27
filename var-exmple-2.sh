20f0209d253caf27b3a286f101f676e4456bca1d
alin_alok <alokmahtab88@gmail.com>
2021-11-27 23:48:41 +0600

    variable example with if else

--- var-exmple-2.sh
+++ var-exmple-2.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
  read -p "Enter your name: " NAME
 echo "Hello $NAME, nice to meet you!"
+
+if [ "$NAME" == "alok" ]
+then
+echo "Your name is Alok"
+else
+echo "Your name is NOT $NAME"
+fi
